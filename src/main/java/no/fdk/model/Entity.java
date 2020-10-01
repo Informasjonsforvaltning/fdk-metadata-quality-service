842aa92ad82e09f041924b7a7d672e570f431f74
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-10-01 17:23:39 +0200

    chore: annotate data classes to filter out null values

--- src/main/java/no/fdk/model/Entity.java
+++ src/main/java/no/fdk/model/Entity.java
@@ -1,10 +1,12 @@
 package no.fdk.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Entity {
     private String uri;
     private EntityType type;
