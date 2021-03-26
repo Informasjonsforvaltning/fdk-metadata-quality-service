73f07681beed0b4ec81217c561670987d1dc1fad
Alexander Zakharov <alzak@envisage.no>
2021-03-26 14:47:22 +0100

    feat: #37 change data source to fdk-sparql-service

---
+++ src/main/java/no/fdk/service/SparqlService.java
@@ -0,0 +1,32 @@
+package no.fdk.service;
+
+import lombok.RequiredArgsConstructor;
+import no.fdk.configuration.ApplicationProperties;
+import no.fdk.model.EntityType;
+import org.apache.jena.graph.Graph;
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionFactory;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+public class SparqlService {
+
+    private final ApplicationProperties applicationProperties;
+
+    public Mono<Graph> getGraph(EntityType entityType) {
+        String url = applicationProperties.getSparqlUri();
+
+        try (RDFConnection connection = RDFConnectionFactory.connectFuseki(url)) {
+            return Mono.justOrEmpty(
+                connection
+                    .queryDescribe(format("DESCRIBE ?s ?p ?o FROM <%s> WHERE { ?s ?p ?o }", entityType.getGraphName()))
+                    .getGraph()
+            );
+        }
+    }
+
+}
