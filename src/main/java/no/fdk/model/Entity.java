6503586763a8f3088f1272015fc113ef04482e72
Alexander Zakharov <alzak@envisage.no>
2021-04-15 15:07:27 +0200

    feat: #40 add entity title

--- src/main/java/no/fdk/model/Entity.java
+++ src/main/java/no/fdk/model/Entity.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 @Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Entity {
     private final String uri;
     private final EntityType type;
+    private final Map<String, String> title;
     private final Catalog catalog;
 }
